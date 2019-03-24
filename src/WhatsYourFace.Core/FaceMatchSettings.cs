7ac55ec46fd3f7ee04c919d7faa19d0e7c6d8a81
Vlad Cananau <vlad.cananau@hotmail.com>
2019-03-24 18:18:08 +0200

    Add FxCop Analysis to all projects

--- src/WhatsYourFace.Core/FaceMatchSettings.cs
+++ src/WhatsYourFace.Core/FaceMatchSettings.cs
@@ -12,7 +12,9 @@ namespace WhatsYourFace.Core
 
         public string FaceListNameFormat { get; set; }
 
+#pragma warning disable CA2227 // Collection properties should be read only
         public List<string> SupportedCountryCodes { get; set; }
+#pragma warning restore CA2227 // Collection properties should be read only
 
         public int MaxImageSizeInBytes { get; set; }
     }
